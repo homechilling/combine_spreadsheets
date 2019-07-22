14776c5e714f11886d1175f1741785a5b8d3ac1a
glottal <hzhang20@binghamton.edu>
2019-07-22 14:35:33 -0400

    improved code's readability

--- combine_csv.py
+++ combine_csv.py
@@ -14,7 +14,7 @@ import pandas as pd
 df1 = pd.read_csv("CSV1.csv") # first file
 df2 = pd.read_csv("CSV2.csv") # second file
 
-df_result = pd.merge(df1, df2, left_on = ['First Name', 'Last Name'], right_on = ['First Name:', 'Last Name:']) # merge two files on first/last names
+df_result = pd.merge(df1, df2, left_on = ['Last Name', 'First Name'], right_on = ['Last Name:', 'First Name:']) # merge two files on first/last names
 df_result.drop('First Name:', axis=1, inplace=True) # remove redundant column
 df_result.drop('Last Name:', axis=1, inplace=True) # remove redundant column
 
